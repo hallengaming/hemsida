1a7ffce6dc5e7199fae307293e97616aa30c5449
Jackohass <Jacob.brannstrom@telia.com>
2017-08-24 10:29:32 +0200

    Delete my own database access information.

--- loggain/config.php
+++ loggain/config.php
@@ -1,8 +1,8 @@
 <?php
-define('mail_password', 'localhost:3306');
+define('mail_password', '');
 define('DB_SERVER', '');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', '');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'hallentest');
+define('DB_DATABASE', '');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
